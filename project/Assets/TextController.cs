2c0ebd120bf77448213266126f8c67ed6cd09945
jipmoon <hyungjip@Hyungs-MacBook-Pro-2.local>
2016-11-22 22:51:16 -0800

    Initial Commit

---
+++ project/Assets/TextController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+	
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello, world!";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKeyDown (KeyCode.Space)) {
+			text.text = "Why don't you work harder instead of complaining " +
+			 			"Yesterday you said tomorrow, stop making execuses and start producing " +
+			 			"It is about time to start building things and it may take a while to see the results.\n\n" +
+			 			"Press S to view Sheets, M to view Mirror and L to view Lock";
+		}
+		
+	}
+}
